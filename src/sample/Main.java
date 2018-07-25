1c6c0a587ff2153a4d01fb12957a2d25f5aa7728
Nazar <baksnaz@gmail.com>
2018-07-25 15:14:54 +0300

    first commit

---
+++ src/sample/Main.java
@@ -0,0 +1,28 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("calculator.css");
+        primaryStage.setTitle("Calculator");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+
+}
